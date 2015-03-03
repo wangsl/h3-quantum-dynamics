be988b22260ebdab5d49f149759e1a9a1af6a754
wangsl2001@gmail.com <wangsl2001@gmail.com@8a8b68d8-4b89-3a71-0164-1f6855a155eb>
2015-03-03 18:39:10 +0000

    add BKMP2 test

---
+++ trunk/BKMP2Test.m
@@ -0,0 +1,34 @@
+
+% $Id$
+
+clear all
+%close all
+
+clc
+format long
+
+global masses
+
+ElectronMass = 9.10938291E-31;
+AMU = 1.66053892E-27;
+MassAU = AMU/ElectronMass;
+
+mH = 1.007825*MassAU;
+mD = 2.01410178*MassAU;
+mT = 3.0160492*MassAU;
+
+masses = [ mH mH mH ];
+
+R = linspace(0.4, 14.0, 256);
+r = linspace(0.4, 8.0, 256);
+
+V = H3CollinearJacobiPES(R, r);
+
+[ ~, hPES ] = contour(R, r, V', [ -0.1:0.01:-0.001 0.001:0.01:0.4 ]);
+set(hPES, 'LineWidth', 0.75);
+set(hPES, 'LineColor', 'black');
+colorbar('vert');
+%axis square
+%hold on;
+
+
---
+++ trunk/svn.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+function svnId()
+{
+    for f in $*; do
+	svn propset svn:keywords "Id" $f
+    done
+}
+
+svn add *.[FChm] *.pl *.sh Makefile-BKMP2Mex.make
+
+svnId add *.[FChm] *.pl *.sh Makefile-BKMP2Mex.make
+
