46e55abe5a50530bd69fd2213d27953a4894c085
wangsl2001@gmail.com <wangsl2001@gmail.com@8a8b68d8-4b89-3a71-0164-1f6855a155eb>
2015-03-05 02:30:37 +0000

    fixed Jacobi coordinates

---
+++ trunk/H3JacobiPES.m
@@ -0,0 +1,34 @@
+
+% $Id$
+
+function [ V ] = H3JacobiPES(R, r, Theta)
+
+global masses
+
+f2 = masses(2)/(masses(2)+masses(3));
+f3 = 1 - f2;
+
+nR = length(R);
+nr = length(r);
+nTheta = length(Theta);
+
+VMax = 4.0;
+rMin = 0.4;
+
+V = zeros(nR, nr, nTheta);
+V = V';
+V(:,:,:) = VMax;
+
+[x, y, z] = meshgrid(R, r, Theta);
+
+r23 = y;
+r12 = sqrt((f2*y).^2 + x.^2 - 2*f2*y.*x.*cos(z));
+r13 = sqrt((f3*y).^2 + x.^2 + 2*f3*y.*x.*cos(z));
+
+Grids = find(r12>rMin & r13>rMin & r23>rMin);
+V(Grids) = BKMP2(r12(Grids), r23(Grids), r13(Grids));
+
+V = V';
+
+return
+
