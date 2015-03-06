9bbf11489a46915f9ea0ddf1632de018fd297b93
wangsl2001@gmail.com <wangsl2001@gmail.com@8a8b68d8-4b89-3a71-0164-1f6855a155eb>
2015-03-06 16:37:57 +0000

    fixed H2Wavefunction.m

--- H2WavefunctionTest.m
+++ H2WavefunctionTest.m
@@ -14,8 +14,6 @@ setenv('OMP_NUM_THREADS', '20');
 
 % UseLSTH = true;
 
-%ElectronMass = 9.10938291E-31;
-%AMU = 1.66053892E-27;
 MassAU = 1.822888484929367e+03;
 
 mH = 1.007825*MassAU;
@@ -26,12 +24,12 @@ masses = [ mH mH mH ];
 
 vH2Min = -0.174495770896975;
 
-r.n = 8192;
+r.n = 4096;
 r.r = linspace(0.4, 16.0, r.n);
 r.dr = r.r(2) - r.r(1);
 r.mass = mH/2;
 
-energy_levels = [ 0 1 10 13 14 15 16 ];
+energy_levels = [ 0 1 6 13 14 15 16 ];
 
 [ e, psi ] = H2Wavefunction(r, energy_levels);
 
@@ -41,27 +39,3 @@ legend(arrayfun(@num2str, energy_levels, 'UniformOutput', false));
 print -dpdf H2-wavefunction.pdf
 
 return
-
-
-
-return
-
-
-R = linspace(0.45, 14.0, 512);
-r = linspace(0.45, 8.0, 512);
-Theta = linspace(0.0, pi, 180);
-
-tic
-V = H3PESJacobi(R, r, Theta);
-toc
-
-V1 = V(:, :, 1);
-
-[ ~, hPES ] = contour(R, r, V1', [ -0.1:0.01:-0.001 0.001:0.01:0.4 ]);
-set(hPES, 'LineWidth', 0.75);
-set(hPES, 'LineColor', 'black');
-colorbar('vert');
-%axis square
-%hold on;
-
-
