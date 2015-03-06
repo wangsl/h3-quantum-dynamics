43009798e8703e22af34489e73a4fd6d2dd50e93
wangsl2001@gmail.com <wangsl2001@gmail.com@8a8b68d8-4b89-3a71-0164-1f6855a155eb>
2015-03-06 19:40:15 +0000

    add H2VibRotWaveFunction.m

---
+++ trunk/H2VibRotTest.m
@@ -0,0 +1,66 @@
+
+% $Id$
+
+clear all
+%close all
+
+clc
+format long
+
+global masses
+global UseLSTH
+
+setenv('OMP_NUM_THREADS', '20');
+
+% UseLSTH = true;
+
+MassAU = 1.822888484929367e+03;
+
+mH = 1.007825*MassAU;
+mD = 2.01410178*MassAU;
+mT = 3.0160492*MassAU;
+
+masses = [ mH mH mH ];
+
+vH2Min = -0.174495770896975;
+
+r.n = 4096;
+r.r = linspace(0.4, 16.0, r.n);
+r.dr = r.r(2) - r.r(1);
+r.mass = mH/2;
+
+R = r.r;
+mu = r.mass;
+
+%jRot = 0;
+%plot(R, H2PES(R)' + jRot*(jRot+1)./(2*mu*R.^2));
+%axis([min(R) max(R) 0 0.2]);
+%return
+
+energy_levels = 0:1:15; %[ 0 1 6 13 14 15 16 ];
+
+[ e, psi ] = H2VibRotWaveFunction(r, 5, energy_levels);
+
+plot(r.r, psi, 'LineWidth', 2);
+legend(arrayfun(@num2str, energy_levels, 'UniformOutput', false));
+
+print -dpdf H2-VibRot.pdf
+
+return
+
+  -0.156641476303803
+  -0.138080296682007
+  -0.120583361684047
+  -0.104130771802557
+  -0.088711613237419
+  -0.074324899327577
+  -0.060980960361705
+  -0.048703468324473
+  -0.037532360745080
+  -0.027528054100074
+  -0.018777594858582
+  -0.011403986746862
+  -0.005581378529287
+  -0.001563620937494
+   0.000166134772494
+   0.000246533536189
--- trunk/H2WaveFunctionTest.m
+++ trunk/H2WaveFunctionTest.m
@@ -29,9 +29,9 @@ r.r = linspace(0.4, 16.0, r.n);
 r.dr = r.r(2) - r.r(1);
 r.mass = mH/2;
 
-energy_levels = [ 0 1 6 13 14 15 16 ];
+energy_levels = 0:1:15; %[ 0 1 6 13 14 15 16 ];
 
-[ e, psi ] = H2Wavefunction(r, energy_levels);
+[ e, psi ] = H2WaveFunction(r, energy_levels);
 
 plot(r.r, psi, 'LineWidth', 2);
 legend(arrayfun(@num2str, energy_levels, 'UniformOutput', false));
@@ -39,3 +39,20 @@ legend(arrayfun(@num2str, energy_levels, 'UniformOutput', false));
 print -dpdf H2-wavefunction.pdf
 
 return
+
+  -0.164566592182935
+  -0.145607293917483
+  -0.127720805943968
+  -0.110884360261777
+  -0.095083828470802
+  -0.080314527140222
+  -0.066582402807662
+  -0.053905749601210
+  -0.042317668977451
+  -0.031869560175718
+  -0.022636068473634
+  -0.014722194285049
+  -0.008273764988278
+  -0.003493015452886
+  -0.000659266463932
+   0.000036144518170
