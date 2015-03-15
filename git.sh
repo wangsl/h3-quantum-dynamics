9ef53a6856b102a7da2b2580d0471b6f5a32aa38
wangsl <wangsl2001@gmail.com>
2015-03-15 13:22:01 -0400

    add git.sh

---
+++ git.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# $Id$
+
+function svnId()
+{
+    for f in $*; do
+	svn propset svn:keywords "Id" $f
+    done
+}
+
+git add *.[FChm] *.pl *.sh *.inc Makefile
+
+#svnId *.[FChm] *.pl *.sh *.inc Makefile
+
+exit
+
+
+
+
+
+
+
+
--- main.m
+++ main.m
@@ -23,11 +23,11 @@ masses = [ mH mH mH ];
 
 vH2Min = -0.174495770896975;
 
-time.total_steps = int32(1000);
+time.total_steps = int32(10);
 time.time_step = 10.0;
 time.steps = int32(0);
 
-r1.n = int32(512);
+r1.n = int32(256);
 r1.r = linspace(0.4, 16.0, r1.n);
 r1.dr = r1.r(2) - r1.r(1);
 r1.mass = 2*mH/3;
@@ -35,13 +35,13 @@ r1.r0 = 10.0;
 r1.k0 = 4.0;
 r1.delta = 0.12;
 
-r2.n = int32(512);
+r2.n = int32(256);
 r2.r = linspace(0.4, 16.0, r2.n);
 r2.dr = r2.r(2)-r2.r(1);
 r2.mass = mH/2;
 
-theta.n = int32(180);
-theta.m = int32(120);
+theta.n = int32(100);
+theta.m = int32(80);
 [ theta.x, theta.w ] = GaussLegendre(theta.n);
 
 theta.legendre = LegendreP2(double(theta.m), theta.x);
